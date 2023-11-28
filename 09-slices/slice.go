2f6537e13a8544b5ffac618efdb55e6d0e9671be
iqbal <iqbal@LAPTOP-V14JPHQN.localdomain>
2023-11-28 16:58:36 +0700

    progress

--- 09-slices/slice.go
+++ 09-slices/slice.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"slices"
 )
 
 func main() {
@@ -29,4 +30,29 @@ func main() {
 
 	l := s[2:5]
 	fmt.Println("sl1:", l)
+
+	l = s[:5]
+	fmt.Println("sl2:", l)
+
+	l = s[2:]
+	fmt.Println("sl3:", l)
+
+	t := []string{"g", "h", "i"}
+	fmt.Println("dcl:", t)
+
+	t2 := []string{"g", "h", "i"}
+	if slices.Equal(t, t2) {
+		fmt.Println("t == t2")
+	}
+
+	twoD := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		in := i + 1
+		twoD[i] = make([]int, in)
+		for j := 0; j < in; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d:", twoD)
+
 }
---
+++ 10-maps/note.md
@@ -0,0 +1,11 @@
+Maps are Go's built-in associative data type (sometimes called hashes or dicts in other language)
+
+To create an empty map, use the builtin make: 
+make(map[key-type]val-type).
+
+Set key/value pairs using typical name[key] = val syntax.
+
+Printing a map with e.g. fmt.Println will show all of the key/value pairs.
+
+Get a value for a key with name[key]
+
